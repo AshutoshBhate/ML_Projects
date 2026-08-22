b78f4e566fa2574d1253b313cb4003b20d5f250a
CeLViron <ashutoshbhate7@gmail.com>
2026-08-23 03:49:12 +0530

    NC

--- Potato_Disease_Classification/api/schemas.py
+++ Potato_Disease_Classification/api/schemas.py
@@ -34,6 +34,7 @@ class UserCreateResponse(BaseModel):
     email: EmailStr
     created_at: datetime
     model_config = ConfigDict(from_attributes= True)
+    
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
